8ac73513b0fedb8dfd3dac595cf766b6220b8058
yuta15 <yxyi8udm1eb1us6aea7s@gmail.com>
2026-05-17 22:00:04 +0900

    修正

--- src/app/infra/account/__init__.py
+++ src/app/infra/account/__init__.py
@@ -1,5 +1,5 @@
 from src.app.infra.account.account_basic_settings_repository import AccountBasicSettingsRepository
-from src.app.infra.account.account_identity_read_interface import AccountIdentityReadRepository
+from src.app.infra.account.account_identity_read_repository import AccountIdentityReadRepository
 from src.app.infra.account.account_read_repository import AccountReadRepository
 from src.app.infra.account.account_metadata_repository import AccountMetadataRepository
 from src.app.infra.account.account_repository import AccountRepository
